a856b55f6ed8ee0dfaaa7161d1d0a0d100128aad
Isaac Kelly <isaacmkelly0@gmail.com>
2021-03-01 18:14:51 -0600

    minor changes to test out components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
-function App() {
+import {useState, useEffect} from "react";
+import Board from "./components/Board"
+const App = () => {
+  // const [players, setPLayers] = useState([]);
+  const [survey, setSurvey] = useState(true);
+  // useEffect(()=>{},[])
   return (
     <div className="App">
-      
+      <Board players={["Isaac","Cody", "Jourdan"]}/>
     </div>
   );
 }
