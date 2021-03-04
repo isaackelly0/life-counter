c32b903e8d2c1e4d4acc313e1a985956443636cb
Isaac Kelly <isaacmkelly0@gmail.com>
2021-03-03 22:14:22 -0600

    can add strings for player list

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,6 +1,21 @@
-const Prompt = () => {
+import {useState} from "react";
+import uniqid from "uniqid";
+
+const Prompt = (props) => {
+    const [text, setText] = useState(" ")
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.fn(text)
+    }
     return(
         <div>
+            <ul>
+                {props.list.map(player=>{return(<li key={uniqid()}>{player}</li>)})}    
+            </ul>
+            <form autoComplete="off" onSubmit={handleSubmit}>
+                <input type="text" onChange={e=>setText(e.target.value)} value={text}></input>
+                <button type="submit">click</button>
+            </form>
         </div>
     )
 }
